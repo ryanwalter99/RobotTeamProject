968fa945308953482e02c5038ca1bd49843aea66
gipsonah <gipsonah@rose-hulman.edu>
2018-05-03 08:38:46 -0400

    Person3.File

--- sandbox_motors_new/person3_motors.py
+++ sandbox_motors_new/person3_motors.py
@@ -11,7 +11,7 @@ Authors: David Fisher, David Mutchler and Alex Gipson.
 
 import ev3dev.ev3 as ev3
 import time
-#
+
 
 def test_turn_left_turn_right():
     """
@@ -31,9 +31,10 @@ def test_turn_left_turn_right():
 # Connect two large motors on output ports B and C
     left_motor = ev3.LargeMotor(ev3.OUTPUT_B)
     right_motor = ev3.LargeMotor(ev3.OUTPUT_C)
+    degrees = int(input('type in degrees'))
     speed = int(input('speed between -100 to 100'))
     stoping_action = int(input('type brake, coast, or hold'))
-    
+    seconds = time * 8
 
     # Check that the motors are actually connected
     assert left_motor.connected
@@ -49,27 +50,26 @@ def test_turn_left_turn_right():
         y = int(input('insert degrees'))
         if y == 0:
             break
-        turn_left_seconds()
+        turn_left_by_time(y, speed, stoping_action)
 
     while True:
         y = int(input('insert degrees'))
         if y == 0:
             break
-        turn_left_by_encoders()
+        turn_left_by_encoders(y, speed, stoping_action)
 
 
     while True:
         x = int(input('insert time'))
         if x == 0:
             break
-        right_motor.run_timed(speed_sp=speed * 8, time_sp=x, stop_action=ev3.motor.STOP_ACTION_BRAKE)
-        left_motor.run_timed(speed_sp=speed * 8, time_sp=x, stop_action=ev3.motor.STOP_ACTION_BRAKE)
+        turn_right_seconds(x, speed, stoping_action)
 
     while True:
         y = int(input('insert degrees'))
         if y == 0:
             break
-        turn_right_seconds()
+        turn_right_by_time()
 
     while True:
         y = int(input('insert degrees'))
@@ -93,8 +93,8 @@ def turn_left_seconds(seconds, speed, stop_action):
     assert left_motor.connected
     assert right_motor.connected
 
-    right_motor.run_timed(speed_sp = speed * 8, time_sp = seconds * 1000, stop_action = stop_action)
-    left_motor.run_timed(speed_sp = -speed * 8, time_sp = seconds * 1000, stop_action = stop_action)
+    right_motor.run_timed(speed_sp=speed * 8, time_sp=seconds * 1000, stop_action=stop_action)
+    left_motor.run_timed(speed_sp=-speed * 8, time_sp=seconds * 1000, stop_action=stop_action)
     right_motor.wait_while('running')
     left_motor.wait_while('running')
 
@@ -116,9 +116,10 @@ def turn_left_by_time(degrees, speed, stop_action):
     # Check that the motors are actually connected
     assert left_motor.connected
     assert right_motor.connected
+    seconds = (int(input('how many seconds?')))
 
-    right_motor.run_timed(speed_sp=speed * 8, position_sp=degrees, stop_action=stop_action)
-    left_motor.run_timed(speed_sp=-speed * 8, position_sp=degrees * 1000, stop_action=stop_action)
+    right_motor.run_timed(speed_sp=speed * 8, time_sp=seconds * 1000, poistion_sp=degrees, stop_action=stop_action)
+    left_motor.run_timed(speed_sp=-speed * 8, time_sp=seconds * 1000, position_sp=degrees, stop_action=stop_action)
     right_motor.wait_while('running')
     left_motor.wait_while('running')
 
@@ -138,9 +139,10 @@ def turn_left_by_encoders(degrees, speed, stop_action):
     # Check that the motors are actually connected
     assert left_motor.connected
     assert right_motor.connected
+    seconds = int(input('how many seconds'))
 
-    right_motor.run_timed(speed_sp=speed * 8, position_sp=degrees, stop_action=stop_action)
-    left_motor.run_timed(speed_sp=-speed * 8, position_sp=-degrees, stop_action=stop_action)
+    right_motor.run_timed(speed_sp=speed * 8, time_sp=seconds * 1000, position_sp=degrees, stop_action=stop_action)
+    left_motor.run_timed(speed_sp=-speed * 8, time_sp=seconds * 1000, position_sp=-degrees, stop_action=stop_action)
     right_motor.wait_while('running')
     left_motor.wait_while('running')
 
@@ -155,8 +157,8 @@ def turn_right_seconds(seconds, speed, stop_action):
     assert left_motor.connected
     assert right_motor.connected
 
-    left_motor.run_timed(speed_sp=speed, time_sp=seconds, stop_action=stop_action)
-    right_motor.run_timed(speed_sp=-speed, time_sp=seconds, stop_action=stop_action)
+    left_motor.run_timed(speed_sp=speed, time_sp=seconds * 1000, stop_action=stop_action)
+    right_motor.run_timed(speed_sp=-speed, time_sp=seconds * 1000, stop_action=stop_action)
     right_motor.wait_while('running')
     left_motor.wait_while('running')
 
@@ -170,9 +172,10 @@ def turn_right_by_time(degrees, speed, stop_action):
     # Check that the motors are actually connected
     assert left_motor.connected
     assert right_motor.connected
+    seconds = int(input('how many seconds'))
 
-    right_motor.run_timed(speed_sp=-speed * 8, position_sp=-degrees, stop_action=stop_action)
-    left_motor.run_timed(speed_sp=speed * 8, position_sp=degrees, stop_action=stop_action)
+    right_motor.run_timed(speed_sp=-speed * 8, time_sp=seconds * 1000, position_sp=-degrees, stop_action=stop_action)
+    left_motor.run_timed(speed_sp=speed * 8, time_sp=seconds * 1000, position_sp=degrees, stop_action=stop_action)
     right_motor.wait_while('running')
     left_motor.wait_while('running')
 
@@ -186,9 +189,10 @@ def turn_right_by_encoders(degrees, speed, stop_action):
     # Check that the motors are actually connected
     assert left_motor.connected
     assert right_motor.connected
+    seconds = int(input('how many seconds'))
 
-    right_motor.run_timed(speed_sp=-speed * 8, position_sp=-degrees, stop_action=stop_action)
-    left_motor.run_timed(speed_sp=speed * 8, position_sp=degrees, stop_action=stop_action)
+    right_motor.run_timed(speed_sp=-speed * 8, time_sp=seconds*1000, position_sp=-degrees, stop_action=stop_action)
+    left_motor.run_timed(speed_sp=speed * 8, time_sp=seconds*1000, position_sp=degrees, stop_action=stop_action)
     right_motor.wait_while('running')
     left_motor.wait_while('running')
 
