628a10c8bc05576e990605cc43f62402428a6f67
gipsonah <gipsonah@rose-hulman.edu>
2018-05-14 09:35:20 -0400

    gipson

--- sandbox/Gipson/mqtt/m5_pc_remote_drive.py
+++ sandbox/Gipson/mqtt/m5_pc_remote_drive.py
@@ -112,23 +112,23 @@ def main():
 
 def send_forward(mqtt_client, left_speed_entry, right_speed_entry):
     print("forward")
-    mqtt_client.send_message("drive",[int(left_speed_entry.get()),
-                                      int(right_speed_entry.get())])
+    mqtt_client.send_message("drive", [int(left_speed_entry.get()),
+                                       int(right_speed_entry.get())])
 
 def send_left(mqtt_client, left_speed_entry, right_speed_entry):
     print("left")
-    mqtt_client.send_message("drive",[-int(left_speed_entry.get()),
-                                      int(right_speed_entry.get())])
+    mqtt_client.send_message("drive", [-int(left_speed_entry.get()),
+                                       int(right_speed_entry.get())])
 
 def send_right(mqtt_client, left_speed_entry, right_speed_entry):
     print("right")
-    mqtt_client.send_message("drive",[int(left_speed_entry.get()),
-                                      -int(right_speed_entry.get())])
+    mqtt_client.send_message("drive", [int(left_speed_entry.get()),
+                                       -int(right_speed_entry.get())])
 
 def send_back(mqtt_client,left_speed_entry,right_speed_entry):
     print("backward")
-    mqtt_client.send_message("backward",[int(left_speed_entry.get()),
-                                         int(right_speed_entry.get())])
+    mqtt_client.send_message("backward", [int(left_speed_entry.get()),
+                                          int(right_speed_entry.get())])
 
 def send_stop(mqtt_client):
     print("stop")
